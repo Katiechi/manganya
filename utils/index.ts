7bcb7f55dcae43ecf97dfa985a13094e0afdf41a
Brian <brian.katiechi9@gmail.com>
2023-06-15 19:14:49 +0300

    Made the whole website responsive and functional

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,12 @@
 import { CarProps, FilterProps } from "../types";
 
-export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 3567; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
+const basePricePerDay = 50; // Base rental price per day in dollars
+const mileageFactor = 0.1; // Additional rate per mile driven
+const ageFactor = 0.05; // Additional rate per year of vehicle age
 
+export const calculateCarRent = (city_mpg: number, year: number) => {
   // Calculate additional rate based on mileage and age
-  const mileageRate = city_mpg * mileageFactor;
+  const mileageRate = city_mpg * mileageFactor*1200;
   const ageRate = (new Date().getFullYear() - year) * ageFactor;
 
   // Calculate total rental rate per day
@@ -36,7 +36,9 @@ export const deleteSearchParams = (type: string) => {
   newSearchParams.delete(type.toLocaleLowerCase());
 
   // Construct the updated URL pathname with the deleted search parameter
-  const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+  const newPathname = `${
+    window.location.pathname
+  }?${newSearchParams.toString()}`;
 
   return newPathname;
 };
@@ -60,21 +62,5 @@ export async function fetchCars(filters: FilterProps) {
 
   // Parse the response as JSON
   const result = await response.json();
-
   return result;
 }
-
-export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  const url = new URL("https://cdn.imagin.studio/getimage");
-  const { make, model, year } = car;
-
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
-  url.searchParams.append('angle', `${angle}`);
-
-  return `${url}`;
-} 
